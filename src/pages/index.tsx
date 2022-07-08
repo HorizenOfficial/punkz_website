0fd8d069478c17522837250277b4504127cf4e33
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-08 13:59:29 +0100

    feat: added basic package setup for gatsby

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const index = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            More text here and more efe affaew More text here and more efe
+            affaew
+        </div>
+    )
+}
+
+export default index
