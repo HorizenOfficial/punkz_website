9046f4eb1eb39bbbdf3205d53c378a2c53b96121
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-11 12:36:05 +0100

    chore: added page colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
         "./src/components/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                "punkz-black-1": "#000000",
+                "punkz-white-1": "#FFFFFF",
+                "punkz-white-2": "#E9ECF4",
+                "punkz-pink-1": "#D62464",
+                "punkz-pink-2": "#DE1FA8",
+                "punkz-dark-blue-1": "#080B25",
+            },
+        },
     },
     plugins: ["gatsby-plugin-postcss"],
 }
