83ee0e1f42f7699d95e3170c0ed8531edb418bf5
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-12 15:27:18 +0100

    feat: updated the images to optimized ones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,20 @@
-import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import HomepageHelmet from "../components/feature/homepage/HomepageHelmet"
+import bgImg from "../assets/images/coming-soon.jpeg"
+import bgImgSmall from "../assets/images/coming-soon-small.png"
 
 const Index = () => {
+    const className = "h-full w-full bg-center bg-contain bg-no-repeat"
     return (
         <main className="bg-black h-[100vh]">
             <HomepageHelmet />
-            <StaticImage
-                src="../assets/images/coming-soon.jpeg"
-                alt="PunkZ coming soon"
-                loading="eager"
-                class="hidden lg:block !static !max-h-[100vh] !m-auto"
-                objectFit="contain"
+            <div
+                className={`${className} hidden lg:block`}
+                style={{ backgroundImage: `url(${bgImg})` }}
             />
-
-            <StaticImage
-                src="../assets/images/coming-soon-small.png"
-                alt="PunkZ coming soon"
-                loading="eager"
-                class="block lg:hidden !static !max-h-[100vh] !m-auto"
-                objectFit="contain"
+            <div
+                className={`${className} block lg:hidden bg-cover md:bg-contain`}
+                style={{ backgroundImage: `url(${bgImgSmall})` }}
             />
         </main>
     )
