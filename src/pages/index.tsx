4ec9fac2d69103b30718ce71ed355760a3ec0a7b
Marcos Jesús Santana Pérez <32165693+MarcosJSP@users.noreply.github.com>
2022-07-12 16:49:52 +0100

    Landing metadata (#2)
    * feat: added metadata
    
    * feat: wip
    
    * feat: updated metadata again
    
    * feat: wip
    
    * feat: wip
    
    * feat: removed gatsby plugin image
    
    * feat: updated the images to optimized ones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
 import React from "react"
-import comingSoonImg from "../assets/images/coming-soon.png"
+import HomepageHelmet from "../components/feature/homepage/HomepageHelmet"
+import bgImg from "../assets/images/coming-soon.jpeg"
+import bgImgSmall from "../assets/images/coming-soon-small.png"
 
 const Index = () => {
+    const className = "h-full w-full bg-center bg-contain bg-no-repeat"
     return (
-        <main
-            className="bg-contain bg-center bg-no-repeat bg-black h-[100vh] w-[100vw]"
-            style={{ backgroundImage: `url(${comingSoonImg})` }}
-        />
+        <main className="bg-black h-[100vh]">
+            <HomepageHelmet />
+            <div
+                className={`${className} hidden lg:block`}
+                style={{ backgroundImage: `url(${bgImg})` }}
+            />
+            <div
+                className={`${className} block lg:hidden bg-cover md:bg-contain`}
+                style={{ backgroundImage: `url(${bgImgSmall})` }}
+            />
+        </main>
     )
 }
 
