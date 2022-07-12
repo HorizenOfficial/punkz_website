031469c7695189a5b3df98dbf13c3455cb5b1580
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-12 15:46:03 +0100

    feat: updated the Header so it looks better on bigger resolutions

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,18 +1,20 @@
 import React from "react"
 import Logo from "./Icon/Logo"
-import { ContentContainer } from "./Layout/index"
+import { ContentContainer, SectionContainer } from "./Layout/index"
 
 const Header = () => {
     return (
         <header className="relative z-10">
-            <hr className="border-t-[8px] border-punkz-pink-1" />
-            <ContentContainer>
-                <div className="h-[160px] flex justify-center items-center">
-                    <a href="/" className="mx-auto flex justify-center">
-                        <Logo className="max-w-[80px] lg:max-w-[130px]" />
-                    </a>
-                </div>
-            </ContentContainer>
+            <SectionContainer>
+                <hr className="border-t-[8px] border-punkz-pink-1" />
+                <ContentContainer>
+                    <div className="h-[160px] flex justify-center items-center">
+                        <a href="/" className="mx-auto flex justify-center">
+                            <Logo className="max-w-[80px] lg:max-w-[130px]" />
+                        </a>
+                    </div>
+                </ContentContainer>
+            </SectionContainer>
         </header>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import HomepageHero from "../components/feature/homepage/HomepageHero"
 import Header from "../components/global/Header"
 import { PageContainer } from "../components/global/Layout"
-import { H2 } from "../components/global/Typography"
 
 const Index = () => {
     return (
