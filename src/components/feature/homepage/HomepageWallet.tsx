02151c8a0e85c617d644c781025a898114395b9e
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-13 17:21:11 +0100

    feat: updated mobile css for wallet

--- src/components/feature/homepage/HomepageWallet.tsx
+++ src/components/feature/homepage/HomepageWallet.tsx
@@ -12,28 +12,33 @@ const HomepageWallet: React.FC<
         // eslint-disable-next-line react/jsx-props-no-spreading
         <section {...props}>
             <SectionContainer>
-                <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-6 xl:gap-16">
+                <div className="grid grid-cols-1 md:grid-cols-2 md:gap-10 xl:gap-16">
                     <img
                         src={walletImg}
                         alt="cobalt wallet"
-                        className="order-2 w-full max-h-[700px] object-contain lg:pb-[60px] xl:pb-0"
+                        className="hidden md:w-full md:block md:max-h-[700px] md:object-contain"
                     />
-                    <ContentContainer className="order-1 lg:order-2 md:mt-[50px] lg:!pl-0 lg:mt-[130px] w-full">
+                    <ContentContainer className="md:!pl-0 md:mt-[60px] lg:mt-[130px] w-full">
                         <H2>Wallet</H2>
 
                         <Body1 className="mt-[40px] max-w-[616px]">
                             You need a wallet to store and manage your Punkz
                             token. Currently, only Cobalt Wallet supports Punkz.
                         </Body1>
+
+                        <img
+                            src={walletImg}
+                            alt="cobalt wallet"
+                            className="block md:hidden w-full max-h-[300px] object-contain"
+                        />
+
                         <a
                             href="https://chrome.google.com/webstore/detail/cobalt/hekbjgfncacdinlajhgiakpaieajpfph"
                             target="_blank"
                             rel="noreferrer"
-                            className="block sm:max-w-[343px]"
+                            className="block sm:max-w-[343px] mt-[10px] md:mt-[40px]"
                         >
-                            <Button1 className="mt-[40px]">
-                                Download Cobalt
-                            </Button1>
+                            <Button1>Download Cobalt</Button1>
                         </a>
                     </ContentContainer>
                 </div>
