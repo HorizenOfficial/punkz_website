fc3e4a47a35b943fc0d01d3a29457c37ed5eadb3
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-13 13:46:00 +0100

    feat: added button styling

--- src/components/global/Button/Button1.tsx
+++ src/components/global/Button/Button1.tsx
@@ -5,7 +5,7 @@ const Button1: React.FC<ButtonProps> = ({ children, className, ...props }) => {
     return (
         <button
             type="button"
-            className={`transition-colors w-full max-w-[min(343px,100%)] h-[67px] border-2 flex justify-center items-center font-proxima font-bold text-[22px] leading-[35px] text-punkz-white-1 bg-punkz-black-1 border-punkz-white-1 hover:bg-punkz-pink-4 hover:border-punkz-pink-2 ${
+            className={`transition-colors w-full max-w-full sm:max-w-[min(343px,100%)] h-[67px] border-2 flex justify-center items-center font-proxima font-bold text-[22px] leading-[35px] text-punkz-white-1 bg-punkz-black-1 border-punkz-white-1 hover:bg-punkz-pink-4 hover:border-punkz-pink-2 ${
                 className || ""
             }`}
             // eslint-disable-next-line react/jsx-props-no-spreading
