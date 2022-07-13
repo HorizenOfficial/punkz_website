fb0edd6a41c5552257ca26f9471d5eddb3cbfa34
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-13 15:53:25 +0100

    feat: added faq section (no real copies yet)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import HomepageAirdrops from "../components/feature/homepage/HomepageAirdrops"
+import HomepageDiscord from "../components/feature/homepage/HomepageDiscord"
+import HomepageFAQ from "../components/feature/homepage/HomepageFAQ"
 import HomepageHero from "../components/feature/homepage/HomepageHero"
 import HomepagePunkZToken from "../components/feature/homepage/HomepagePunkZToken"
 import HomepageWallet from "../components/feature/homepage/HomepageWallet"
@@ -14,6 +16,8 @@ const Index = () => {
             <HomepageAirdrops className="pt-[85px]" />
             <HomepageWallet className="pt-10 md:pt-[85px] xl:pt-[150px]" />
             <HomepagePunkZToken className="pt-10 md:pt-[85px] lg:pt-[150px] xl:pt-[200px]" />
+            <HomepageDiscord className="pt-10 md:pt-[85px] lg:pt-[150px] xl:pt-[200px]" />
+            <HomepageFAQ />
         </PageContainer>
     )
 }
