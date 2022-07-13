f50135e4d67b34ae4fdd0a7073d490ccac58128f
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-13 15:51:59 +0100

    feat: added discord section

---
+++ src/components/feature/homepage/HomepageDiscord.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import DiscordLogo from "../../global/Icon/DiscordLogo"
+import { ContentContainer, SectionContainer } from "../../global/Layout"
+import { H2 } from "../../global/Typography"
+
+const HomepageDiscord: React.FC<
+    React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+> = (props) => {
+    return (
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        <section {...props}>
+            <SectionContainer>
+                <div className="bg-punkz-pink-2 py-[73px]">
+                    <ContentContainer>
+                        <a
+                            href="/"
+                            className="flex flex-col justify-center items-center"
+                        >
+                            <DiscordLogo />
+                            <H2 className="!text-5xl mt-[18px]">
+                                Join the Punkz
+                            </H2>
+                        </a>
+                    </ContentContainer>
+                </div>
+            </SectionContainer>
+        </section>
+    )
+}
+
+export default HomepageDiscord
