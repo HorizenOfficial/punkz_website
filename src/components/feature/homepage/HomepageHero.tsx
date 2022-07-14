a2b29e7cefd036211f84bbc35b1bb35348799029
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-14 18:30:12 +0100

    feat: added responsiveness for hrero section

--- src/components/feature/homepage/HomepageHero.tsx
+++ src/components/feature/homepage/HomepageHero.tsx
@@ -2,9 +2,11 @@ import React from "react"
 import HomepageHeroFrame from "../../global/Icon/HomepageHeroFrame"
 import { ContentContainer, SectionContainer } from "../../global/Layout"
 import heroImg from "../../../assets/images/hero.png"
+import heroSmallImg from "../../../assets/images/hero-small.png"
 import HomepageHeroLogo from "../../global/Icon/HomepageHeroLogo"
 import HomepageHeroLinkBox from "../../global/Icon/HomepageHeroLinkBox"
 import HomepageHeroLinkArrow from "../../global/Icon/HomepageHeroLinkArrow"
+import { Body1 } from "../../global/Typography"
 
 const HomepageHero = () => {
     const renderLinks = (): JSX.Element[] => {
@@ -26,9 +28,13 @@ const HomepageHero = () => {
 
         links.forEach((link) =>
             elements.push(
-                <a href={link.href} className="group w-[160px]" key={link.text}>
-                    <span className="font-proxima font-bold text-[18px] transition-all group-hover:ml-2 flex gap-5 items-center text-[#B4BBCC] hover:text-white">
-                        <HomepageHeroLinkArrow className="transition-colors stroke-[#B4BBCC] group-hover:stroke-punkz-pink-2" />
+                <a
+                    href={link.href}
+                    key={link.text}
+                    className="group w-[160px] transition-all lg:hover:ml-2 flex items-center flex-col lg:flex-row lg:gap-x-5"
+                >
+                    <HomepageHeroLinkArrow className="transition-colors stroke-punkz-pink-2 rotate-90 lg:rotate-0 lg:stroke-[#B4BBCC] group-hover:stroke-punkz-pink-2" />
+                    <span className="font-proxima font-bold text-[18px] text-white lg:text-[#B4BBCC] hover:text-white">
                         {link.text}
                     </span>
                 </a>
@@ -38,33 +44,33 @@ const HomepageHero = () => {
     }
 
     return (
-        <section className="-mt-[80px] z-0 relative">
+        <section>
             <SectionContainer>
-                <img
-                    src={heroImg}
-                    alt="hero-img"
-                    className="m-auto w-full max-h-[min(90vh,900px)] object-fill"
-                />
+                <div className="hidden -mt-[80px] z-0 relative lg:block">
+                    <img
+                        src={heroImg}
+                        alt="hero-img"
+                        className="m-auto w-full max-h-[min(90vh,900px)] object-fill"
+                    />
 
-                <div className="absolute grid grid-cols-2 inset-0">
-                    <div className="bg-punkz-black-1/50" />
-                    <div />
-                </div>
+                    <div className="absolute grid grid-cols-2 inset-0">
+                        <div className="bg-punkz-black-1/50 backdrop-blur-[7px]" />
+                        <div />
+                    </div>
 
-                <ContentContainer className="absolute inset-0">
                     <div className="absolute inset-0">
                         <HomepageHeroFrame
                             width="100%"
-                            className="absolute inset-x-auto top-[55px] z-10 px-[80px] xl:px-0"
+                            className="absolute inset-x-auto top-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
                         />
                         <HomepageHeroFrame
                             width="100%"
-                            className="absolute rotate-180 inset-x-auto bottom-[55px] z-10 px-[80px] xl:px-0"
+                            className="absolute rotate-180 inset-x-auto bottom-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
                         />
 
-                        <div className="absolute inset-0 grid gap-[100px] grid-cols-2 items-center justify-items-center px-[120px] xl:px-[80px]">
-                            <div className="flex flex-col items-center">
-                                <HomepageHeroLogo className="max-w-[456px] h-full w-full" />
+                        <div className="absolute inset-0 grid gap-[100px] grid-cols-2 items-center justify-items-center max-w-screen-xl mx-auto px-6 md:px-12 lg:px-8 xl:px-14">
+                            <div className="flex flex-col items-center max-w-[456px] pl-[40px] xl:pl-0">
+                                <HomepageHeroLogo className="h-full w-full" />
                                 <p className="font-proxima text-punkz-white-1 font-bold mt-[20px] text-[22px] xl:text-[26px]">
                                     Punkz Token (PKZ) is a meme token for all
                                     cypherpunk rockstars in the galaxy.
@@ -79,7 +85,37 @@ const HomepageHero = () => {
                             </div>
                         </div>
                     </div>
-                </ContentContainer>
+                </div>
+
+                <div className="block lg:hidden">
+                    <ContentContainer>
+                        <div className="flex flex-col items-center justify-center">
+                            <HomepageHeroFrame className="w-full flex" />
+                            <HomepageHeroLogo className="w-[240px] h-[137px] relative z-10 mt-[15px] md:mt-[100px]" />
+                            <Body1 className="!font-bold max-w-[400px] !text-center mt-4 relative z-10">
+                                Punkz Token (PKZ) is a meme token for all
+                                cypherpunk rockstars in the galaxy.
+                            </Body1>
+                            <div className="relative !max-w-[100vw] w-[100vw] -mt-7 z-0 md:-mt-[280px]">
+                                <img
+                                    src={heroImg}
+                                    alt="hero-img"
+                                    className="hidden md:block max-h-[400px] object-fill w-full opacity-80"
+                                />
+                                <img
+                                    src={heroSmallImg}
+                                    alt="hero-img"
+                                    className="block md:hidden max-h-[400px] object-fill w-full"
+                                />
+                            </div>
+
+                            <div className="flex flex-col items-center justify-center -mt-9 relative z-10 gap-5 md:flex-row">
+                                {renderLinks()}
+                            </div>
+                            <HomepageHeroFrame className="rotate-180 w-full mt-11" />
+                        </div>
+                    </ContentContainer>
+                </div>
             </SectionContainer>
         </section>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Index = () => {
         <PageContainer>
             <Header />
             <HomepageHero />
-            <HomepageAirdrops className="pt-[85px]" />
+            <HomepageAirdrops className="pt-[43px] lg:pt-[85px]" />
             <HomepageWallet className="pt-[118px] lg:pt-[150px] xl:pt-[150px]" />
             <HomepagePunkZToken className="pt-[118px] lg:pt-[150px] xl:pt-[200px]" />
             <HomepageDiscord className="pt-[118px] lg:pt-[150px] xl:pt-[200px]" />
