e08f06792b9ab0324e4e1759aae995b967d83755
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-15 11:17:57 +0100

    fix: optimised the way we generate date boxes

--- src/components/feature/homepage/HomepageAirdrops.tsx
+++ src/components/feature/homepage/HomepageAirdrops.tsx
@@ -7,7 +7,6 @@ const HomepageAirdrops: React.FC<
     React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
 > = (props) => {
     const renderDates = (): JSX.Element[] => {
-        const elements: JSX.Element[] = []
         const dates = [
             {
                 text: "PKZ drop to 1st group of TZNY holders ",
@@ -27,9 +26,9 @@ const HomepageAirdrops: React.FC<
             },
         ]
 
-        dates.forEach((date) => {
+        return dates.map((date) => {
             const isPastDate = new Date(date.date) < new Date()
-            elements.push(
+            return (
                 <div
                     className={`${
                         isPastDate ? "bg-punkz-pink-3" : "bg-punkz-black-1"
@@ -52,7 +51,6 @@ const HomepageAirdrops: React.FC<
                 </div>
             )
         })
-        return elements
     }
 
     return (
