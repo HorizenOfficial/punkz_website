a8d0ee49e045c80c6d38fa38827d858a47bdccd6
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-18 13:29:06 +0100

    feat: updated the airdrops section design to fit the latest design

--- src/components/feature/homepage/HomepageAirdropsV1.tsx
+++ src/components/feature/homepage/HomepageAirdropsV1.tsx
@@ -1,42 +1,9 @@
 import React from "react"
+import TwitterLogoSmall from "../../global/Icon/TwitterLogoSmall"
 import { SectionContainer } from "../../global/Layout"
 import { Body1, H2 } from "../../global/Typography"
-
-interface BoxProps {
-    children: React.ReactNode
-    className: string
-    innerClassName: string
-}
-
-const PinkBox: React.FC<BoxProps> = ({
-    children,
-    className,
-    innerClassName,
-}) => {
-    return (
-        <div className={`bg-punkz-pink-2 py-[40px] lg:py-[85px] ${className}`}>
-            <div
-                className={`flex max-w-[750px] flex-col gap-7 ${innerClassName}`}
-            >
-                {children}
-            </div>
-        </div>
-    )
-}
-
-const BlackBox: React.FC<BoxProps> = ({
-    children,
-    className,
-    innerClassName,
-}) => {
-    return (
-        <div
-            className={`bg-punkz-black-1 pb-[40px] pt-[65px] lg:pt-[85px] ${className}`}
-        >
-            <H2 className={`lg:max-w-[300px] ${innerClassName}`}>{children}</H2>
-        </div>
-    )
-}
+import { BigBox, SmallBox } from "./HomepageAirdropsV1Boxes"
+import { BottomCut, TopCut } from "./HomepageAirdropsV1Cuts"
 
 const HomepageAirdropsV1: React.FC<
     React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
@@ -48,15 +15,17 @@ const HomepageAirdropsV1: React.FC<
         // eslint-disable-next-line react/jsx-props-no-spreading
         <section {...props} id="airdrops">
             <SectionContainer>
-                <div className="grid grid-cols-1 lg:grid-cols-[35fr,65fr] lg:mb-[62px]">
-                    <BlackBox
-                        className={`lg:pr-[77px] ${paddingsLeft}`}
-                        innerClassName="ml-auto lg:text-right"
+                <div className="relative lg:h-[440px] bg-punkz-dark-blue-1 grid grid-cols-1 lg:grid-cols-[35fr,65fr]">
+                    <TopCut className="hidden lg:block absolute top-0 left-0 !w-[20%]" />
+                    <SmallBox
+                        className={`lg:pr-[77px] bg-punkz-pink-2 ${paddingsLeft}`}
                     >
-                        Initial Airdrops
-                    </BlackBox>
+                        <H2 className="lg:max-w-[300px] lg:ml-auto lg:text-right">
+                            Initial Airdrops
+                        </H2>
+                    </SmallBox>
 
-                    <PinkBox
+                    <BigBox
                         className={`lg:pl-[47px] ${paddingsRight}`}
                         innerClassName="lg:mr-auto"
                     >
@@ -64,41 +33,57 @@ const HomepageAirdropsV1: React.FC<
                             TZNY and TCAKE holders as of July 5th 00:00am EST
                             will get a Punkz Token airdrop.
                         </Body1>
-                        <Body1>
+                        <Body1 className="italic !text-punkz-gray-1">
                             Details about the initial airdrops and schedule are
                             coming soon.
                         </Body1>
                         <Body1>
-                            Follow Punkz on Twitter for the latest updates
+                            <TwitterLogoSmall className="w-[26px] h-[21px] mr-5 inline-block -mt-1" />
+                            Follow Punkz on
+                            <a
+                                className="underline transition-colors hover:text-punkz-pink-2 mx-2 inline-block"
+                                target="_blank"
+                                href="https://twitter.com/Punkz_Token"
+                                rel="noreferrer"
+                            >
+                                Twitter
+                            </a>
+                            for the latest updates.
                         </Body1>
-                    </PinkBox>
+                    </BigBox>
+
+                    <BottomCut className="hidden lg:block absolute right-0 bottom-0 !w-[calc(80%+28px)]" />
                 </div>
-                <div className="grid grid-cols-1 lg:grid-cols-[65fr,35fr]">
-                    <PinkBox
+                <div className="relative lg:h-[440px] bg-punkz-dark-blue-1 grid grid-cols-1 lg:grid-cols-[65fr,35fr]">
+                    <TopCut className="hidden lg:block absolute top-0 left-0 !w-[calc(80%+28px)]" />
+                    <BigBox
                         className={`lg:pr-[47px] order-2 lg:order-1 ${paddingsLeft}`}
                         innerClassName="lg:ml-auto"
                     >
                         <Body1 className="font-bold">
-                            Not TZNY or TCAKE holders? The Punkz Droplist is for
-                            you!
+                            Not TZNY or TCAKE holders?
+                            <br />
+                            The Punkz Droplist is for you!
                         </Body1>
                         <Body1>
                             It is for anyone who has their wallet address
                             pre-approved and meets other set requirements to
                             receive Punkz airdrops.
                         </Body1>
-                        <Body1>
+                        <Body1 className="italic !text-punkz-gray-1">
                             Details about how to get on to the Punkz Droplist
                             are coming soon
                         </Body1>
-                    </PinkBox>
+                    </BigBox>
 
-                    <BlackBox
-                        className={`order-1 lg:order-2 lg:pl-[77px] ${paddingsRight}`}
-                        innerClassName="lg:mr-auto"
+                    <SmallBox
+                        className={`order-1 lg:order-2 lg:pl-[77px] bg-punkz-blue-1 ${paddingsRight}`}
                     >
-                        Punkz Droplist
-                    </BlackBox>
+                        <H2 className="lg:max-w-[300px] lg:mr-auto !text-punkz-black-1">
+                            Punkz Droplist
+                        </H2>
+                    </SmallBox>
+                    <BottomCut className="hidden lg:block absolute right-0 bottom-0 !w-[20%]" />
                 </div>
             </SectionContainer>
         </section>
