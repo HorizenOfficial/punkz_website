233078fbcf3ad368d17fb1fa303d188acf53b2d9
Marcos JSP <marcos@theagilemonkeys.com>
2022-07-25 12:48:13 +0100

    feat: removed SectionContainer & ContentContainer

--- src/components/feature/homepage/HomepageAirdropsV1.tsx
+++ src/components/feature/homepage/HomepageAirdropsV1.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import TwitterLogoSmall from "../../global/Icon/TwitterLogoSmall"
-import { SectionContainer } from "../../global/Layout"
 import { Body1, H2 } from "../../global/Typography"
 import { BigBox, SmallBox } from "./HomepageAirdropsV1Boxes"
 import { BottomCut, TopCut } from "./HomepageAirdropsV1Cuts"
@@ -8,81 +7,79 @@ import { BottomCut, TopCut } from "./HomepageAirdropsV1Cuts"
 const HomepageAirdropsV1: React.FC<
     React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
 > = (props) => {
-    const paddingsLeft = "px-6 md:px-12 lg:pl-8 xl:pl-2"
-    const paddingsRight = "px-6 md:px-12 lg:pr-8 xl:pr-2"
+    const paddingsLeft = "px-6 md:px-12 lg:pl-8"
+    const paddingsRight = "px-6 md:px-12 lg:pr-8"
 
     return (
         // eslint-disable-next-line react/jsx-props-no-spreading
         <section {...props} id="airdrops">
-            <SectionContainer>
-                <div className="relative lg:h-[440px] bg-secondary-dark grid grid-cols-1 lg:grid-cols-[35fr,65fr]">
-                    <TopCut className="hidden lg:block absolute top-0 left-0 w-[20%]" />
-                    <SmallBox
-                        className={`lg:pr-[77px] bg-primary-light ${paddingsLeft}`}
-                    >
-                        <H2 className="lg:max-w-[300px] lg:ml-auto lg:text-right">
-                            Initial Airdrops
-                        </H2>
-                    </SmallBox>
-                    <BigBox
-                        className={`lg:pl-[47px] ${paddingsRight}`}
-                        innerClassName="lg:mr-auto"
-                    >
-                        <Body1 className="font-bold">
-                            TZNY and TCAKE holders as of July 5th 00:00am EST
-                            will get a Punkz Token airdrop.
-                        </Body1>
-                        <Body1 className="italic text-gray">
-                            Details about the initial airdrops and schedule are
-                            coming soon.
-                        </Body1>
-                        <Body1>
-                            <TwitterLogoSmall className="w-[26px] h-[21px] mr-5 inline-block -mt-1" />
-                            Follow Punkz on
-                            <a
-                                className="underline transition-colors hover:text-primary-light mx-2 inline-block"
-                                target="_blank"
-                                href="https://twitter.com/Punkz_Token"
-                                rel="noreferrer"
-                            >
-                                Twitter
-                            </a>
-                            for the latest updates.
-                        </Body1>
-                    </BigBox>
-                    <BottomCut className="hidden lg:block absolute right-0 bottom-0 w-[calc(80%+28px)]" />
-                </div>
-                <div className="relative lg:h-[440px] bg-secondary-dark grid grid-cols-1 lg:grid-cols-[65fr,35fr]">
-                    <TopCut className="hidden lg:block absolute top-0 left-0 w-[calc(80%+28px)]" />
-                    <BigBox
-                        className={`lg:pr-[47px] order-2 lg:order-1 ${paddingsLeft}`}
-                        innerClassName="lg:ml-auto"
-                    >
-                        <Body1 className="font-bold">
-                            Not TZNY or TCAKE holders?
-                            <br />
-                            The Punkz Droplist is for you!
-                        </Body1>
-                        <Body1>
-                            It is for anyone who has their wallet address
-                            pre-approved and meets other set requirements to
-                            receive Punkz airdrops.
-                        </Body1>
-                        <Body1 className="italic text-gray">
-                            Details about how to get on to the Punkz Droplist
-                            are coming soon
-                        </Body1>
-                    </BigBox>
-                    <SmallBox
-                        className={`order-1 lg:order-2 lg:pl-[77px] bg-secondary ${paddingsRight}`}
-                    >
-                        <H2 className="lg:max-w-[300px] lg:mr-auto text-black">
-                            Punkz Droplist
-                        </H2>
-                    </SmallBox>
-                    <BottomCut className="hidden lg:block absolute right-0 bottom-0 w-[20%]" />
-                </div>
-            </SectionContainer>
+            <div className="relative lg:h-[440px] bg-secondary-dark grid grid-cols-1 lg:grid-cols-[35fr,65fr]">
+                <TopCut className="hidden lg:block absolute top-0 left-0 w-[20%]" />
+                <SmallBox
+                    className={`lg:pr-[77px] bg-primary-light ${paddingsLeft}`}
+                >
+                    <H2 className="lg:max-w-[300px] lg:ml-auto lg:text-right">
+                        Initial Airdrops
+                    </H2>
+                </SmallBox>
+                <BigBox
+                    className={`lg:pl-[47px] ${paddingsRight}`}
+                    innerClassName="lg:mr-auto"
+                >
+                    <Body1 className="font-bold">
+                        TZNY and TCAKE holders as of July 5th 00:00am EST will
+                        get a Punkz Token airdrop.
+                    </Body1>
+                    <Body1 className="italic text-gray">
+                        Details about the initial airdrops and schedule are
+                        coming soon.
+                    </Body1>
+                    <Body1>
+                        <TwitterLogoSmall className="w-[26px] h-[21px] mr-5 inline-block -mt-1" />
+                        Follow Punkz on
+                        <a
+                            className="underline transition-colors hover:text-primary-light mx-2 inline-block"
+                            target="_blank"
+                            href="https://twitter.com/Punkz_Token"
+                            rel="noreferrer"
+                        >
+                            Twitter
+                        </a>
+                        for the latest updates.
+                    </Body1>
+                </BigBox>
+                <BottomCut className="hidden lg:block absolute right-0 bottom-0 w-[calc(80%+28px)]" />
+            </div>
+            <div className="relative lg:h-[440px] bg-secondary-dark grid grid-cols-1 lg:grid-cols-[65fr,35fr]">
+                <TopCut className="hidden lg:block absolute top-0 left-0 w-[calc(80%+28px)]" />
+                <BigBox
+                    className={`lg:pr-[47px] order-2 lg:order-1 ${paddingsLeft}`}
+                    innerClassName="lg:ml-auto"
+                >
+                    <Body1 className="font-bold">
+                        Not TZNY or TCAKE holders?
+                        <br />
+                        The Punkz Droplist is for you!
+                    </Body1>
+                    <Body1>
+                        It is for anyone who has their wallet address
+                        pre-approved and meets other set requirements to receive
+                        Punkz airdrops.
+                    </Body1>
+                    <Body1 className="italic text-gray">
+                        Details about how to get on to the Punkz Droplist are
+                        coming soon
+                    </Body1>
+                </BigBox>
+                <SmallBox
+                    className={`order-1 lg:order-2 lg:pl-[77px] bg-secondary ${paddingsRight}`}
+                >
+                    <H2 className="lg:max-w-[300px] lg:mr-auto text-black">
+                        Punkz Droplist
+                    </H2>
+                </SmallBox>
+                <BottomCut className="hidden lg:block absolute right-0 bottom-0 w-[20%]" />
+            </div>
         </section>
     )
 }
--- src/components/feature/homepage/HomepageHero.tsx
+++ src/components/feature/homepage/HomepageHero.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import HomepageHeroFrame from "../../global/Icon/HomepageHeroFrame"
-import { ContentContainer, SectionContainer } from "../../global/Layout"
 import heroImg from "../../../assets/images/hero.png"
 import heroSmallImg from "../../../assets/images/hero-small.png"
 import HomepageHeroLogo from "../../global/Icon/HomepageHeroLogo"
@@ -41,78 +40,74 @@ const HomepageHero = () => {
 
     return (
         <section>
-            <SectionContainer>
-                <div className="hidden -mt-[80px] z-0 relative lg:block">
-                    <img
-                        src={heroImg}
-                        alt="hero-img"
-                        className="m-auto w-full max-h-[min(90vh,900px)] object-fill"
+            <div className="hidden -mt-[80px] z-0 relative lg:block">
+                <img
+                    src={heroImg}
+                    alt="hero-img"
+                    className="m-auto w-full max-h-[min(90vh,900px)] object-fill"
+                />
+
+                <div className="absolute grid grid-cols-2 inset-0">
+                    <div className="bg-black/50 backdrop-blur-[7px]" />
+                    <div />
+                </div>
+
+                <div className="absolute inset-0">
+                    <HomepageHeroFrame
+                        width="100%"
+                        className="absolute inset-x-auto top-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
+                    />
+                    <HomepageHeroFrame
+                        width="100%"
+                        className="absolute rotate-180 inset-x-auto bottom-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
                     />
 
-                    <div className="absolute grid grid-cols-2 inset-0">
-                        <div className="bg-black/50 backdrop-blur-[7px]" />
-                        <div />
-                    </div>
-
-                    <div className="absolute inset-0">
-                        <HomepageHeroFrame
-                            width="100%"
-                            className="absolute inset-x-auto top-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
-                        />
-                        <HomepageHeroFrame
-                            width="100%"
-                            className="absolute rotate-180 inset-x-auto bottom-[55px] z-10 px-6 md:px-12 lg:px-8 xl:px-2"
-                        />
-
-                        <div className="absolute inset-0 grid gap-[100px] grid-cols-2 items-center justify-items-center max-w-screen-xl mx-auto px-6 md:px-12 lg:px-8 xl:px-14">
-                            <div className="flex flex-col items-center max-w-[456px] pl-[40px] xl:pl-0">
-                                <HomepageHeroLogo className="h-full w-full" />
-                                <p className="font-proxima text-white font-bold mt-[20px] text-[22px] xl:text-[26px]">
-                                    Punkz Token (PKZ) is a meme token for all
-                                    cypherpunk rockstars in the galaxy.
-                                </p>
-                            </div>
-
-                            <div className="h-[223px] w-[330px]">
-                                <HomepageHeroLinkBox className="absolute inset z-0" />
-                                <div className="relative z-1 h-full flex flex-col gap-[30px] justify-center items-center ">
-                                    {renderLinks()}
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="block lg:hidden">
-                    <ContentContainer>
-                        <div className="flex flex-col items-center justify-center">
-                            <HomepageHeroFrame className="w-full flex" />
-                            <HomepageHeroLogo className="w-[240px] h-[137px] relative z-10 mt-[15px] md:mt-[100px]" />
-                            <Body1 className="font-bold max-w-[400px] text-center mt-4 relative z-10">
+                    <div className="absolute inset-0 grid gap-[100px] grid-cols-2 items-center justify-items-center max-w-screen-xl mx-auto px-6 md:px-12 lg:px-8 xl:px-14">
+                        <div className="flex flex-col items-center max-w-[456px] pl-[40px] xl:pl-0">
+                            <HomepageHeroLogo className="h-full w-full" />
+                            <p className="font-proxima text-white font-bold mt-[20px] text-[22px] xl:text-[26px]">
                                 Punkz Token (PKZ) is a meme token for all
                                 cypherpunk rockstars in the galaxy.
-                            </Body1>
-                            <div className="relative max-w-[100vw] w-[100vw] -mt-7 z-0 md:-mt-[280px]">
-                                <img
-                                    src={heroImg}
-                                    alt="hero-img"
-                                    className="hidden md:block max-h-[400px] object-fill w-full opacity-80"
-                                />
-                                <img
-                                    src={heroSmallImg}
-                                    alt="hero-img"
-                                    className="block md:hidden max-h-[400px] object-fill w-full"
-                                />
-                            </div>
+                            </p>
+                        </div>
 
-                            <div className="flex flex-col items-center justify-center -mt-9 relative z-10 gap-5 md:flex-row">
+                        <div className="h-[223px] w-[330px]">
+                            <HomepageHeroLinkBox className="absolute inset z-0" />
+                            <div className="relative z-1 h-full flex flex-col gap-[30px] justify-center items-center ">
                                 {renderLinks()}
                             </div>
-                            <HomepageHeroFrame className="rotate-180 w-full mt-11" />
                         </div>
-                    </ContentContainer>
+                    </div>
                 </div>
-            </SectionContainer>
+            </div>
+
+            <div className="block px-6 md:px-12 lg:px-8 lg:hidden">
+                <div className="flex flex-col items-center justify-center">
+                    <HomepageHeroFrame className="w-full flex" />
+                    <HomepageHeroLogo className="w-[240px] h-[137px] relative z-10 mt-[15px] md:mt-[100px]" />
+                    <Body1 className="font-bold max-w-[400px] text-center mt-4 relative z-10">
+                        Punkz Token (PKZ) is a meme token for all cypherpunk
+                        rockstars in the galaxy.
+                    </Body1>
+                    <div className="relative max-w-[100vw] w-[100vw] -mt-7 z-0 md:-mt-[280px]">
+                        <img
+                            src={heroImg}
+                            alt="hero-img"
+                            className="hidden md:block max-h-[400px] object-fill w-full opacity-80"
+                        />
+                        <img
+                            src={heroSmallImg}
+                            alt="hero-img"
+                            className="block md:hidden max-h-[400px] object-fill w-full"
+                        />
+                    </div>
+
+                    <div className="flex flex-col items-center justify-center -mt-9 relative z-10 gap-5 md:flex-row">
+                        {renderLinks()}
+                    </div>
+                    <HomepageHeroFrame className="rotate-180 w-full mt-11" />
+                </div>
+            </div>
         </section>
     )
 }
